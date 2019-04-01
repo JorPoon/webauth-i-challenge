34931169d483301d22e369fff3f84bd492b7318f
Jor_Poon <jormpoon@gmail.com>
2019-04-01 17:36:33 -0400

    Registering new username and password with hash complete

--- routers/users-router.js
+++ routers/users-router.js
@@ -7,6 +7,22 @@ router.get('/', (req, res) => {
     res.send('IT WORKINGGGGG')
 })
 
+router.post('/register', (req, res) => {
+    let user = req.body;
+
+    //hash the password
+    const hash = bcrypt.hashSync(user.password, 8)
+    user.password = hash;
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved)
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        });
+});
+
 
 
 module.exports = router;
